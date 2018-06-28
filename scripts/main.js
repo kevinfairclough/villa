ff6a510bddb9b267e40a89d10b9a7fb2e6b5f289
Kevin Fairclough <kefai@isherwoods.com>
2018-06-28 16:43:36 +0100

    Added jump to navigation when scrolled

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,9 @@
-// $(function () {
+window.onscroll = function() {scrollFunction()};
 
-//     function initMap() {
-
-//         var location = new google.maps.LatLng(50.0875726, 14.4189987);
-
-//         var mapCanvas = document.getElementById('map');
-//         var mapOptions = {
-//             center: location,
-//             zoom: 16,
-//             panControl: false,
-//             mapTypeId: google.maps.MapTypeId.ROADMAP
-//         }
-//         var map = new google.maps.Map(mapCanvas, mapOptions);
-
-//     }
-
-//     google.maps.event.addDomListener(window, 'load', initMap);
-// });
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("top").style.display = "block";
+    } else {
+        document.getElementById("top").style.display = "none";
+    }
+}
